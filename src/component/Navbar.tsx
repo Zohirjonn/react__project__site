03cea129911cf9a7902cf8c6919e45c79c425d00
Zohirjon <example@mail.com>
2023-02-22 19:20:15 +0500

    created react project

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function Navbar(){
+    
+    return(
+        <div className="nabar">
+            <div className="navbar__col"></div>
+            <div className="navbar__col"></div>
+            <div className="navbar__col"></div>
+        </div>
+    )
+}
+export default Navbar
